9f481f7d49f5b5043fc441942825eb9efd90e0ef
mark <mark.kimani3@student.moringaschool.com>
2025-04-14 14:12:12 +0300

    Initial Commit

--- src/data/user.js
+++ src/data/user.js
@@ -4,3 +4,11 @@ const city = "New York";
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
 // export all three variables as named exports
+function getUser() {
+  return {
+    username,
+    city,
+    image,
+  };
+}
+export { username, city, image, getUser };
